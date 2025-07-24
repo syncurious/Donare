9d36daa4efaf1b55ab28af076d9d81096ab5f725
MubbashirHussain <mubbashirhhussain@gmail.com>
2025-07-24 13:15:31 +0500

    Change the App icon

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <ThemeProvider>
         <View style={{flex : 1,paddingTop : Platform.OS == 'ios' ? 30 : 0}}>
-      <MainNavigation />
+            <MainNavigation />
         </View>
     </ThemeProvider>
   );
