65db8d712683e211aeba6f336ec8a6a84c0872f1
AqibIqbal1 <aqib.dev021@gmail.com>
2025-10-29 02:18:31 +0500

    fix error

--- src/config/navigation/AdminNavigation/BottomTabs/index.tsx
+++ src/config/navigation/AdminNavigation/BottomTabs/index.tsx
@@ -4,7 +4,7 @@ import AdminDashboard from '../../../../screens/admin/dashboard';
 import AdminVolunteerApplications from '../../../../screens/admin/volunteers';
 import DonationHistory from '../../../../screens/admin/donation/DonationHistory';
 import HelpRequest from '../../../../screens/admin/Request/HelpRequest';
-import AdminProfile from '../../../../screens/admin/profile';
+import AdminProfile from '../../../../screens/admin/Profile';
 
 const Tab = createBottomTabNavigator();
 const AdminBottomNavigation = () => {
